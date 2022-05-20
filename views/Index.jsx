2c2793afea3dfae40fc45aaa8fbbdf7298bb62c2
TecoraY <102709566+TecoraY@users.noreply.github.com>
2022-05-19 23:17:33 -0400

    I can view a list of all my pokemon in the browser

--- views/Index.jsx
+++ views/Index.jsx
@@ -2,9 +2,22 @@ const React=require('react');
 class Index extends React.Component{
     render(){
         const {pokemon} =this.props;
+        const myStyle = {
+            color: '#ffffff',
+            backgroundColor: '#000000',
+          };
         return(
         <> 
-        <h1> See All The Pokemon!</h1>
+        <h1 style={myStyle}> See All The Pokemon!</h1>
+        <ul style={myStyle}>
+            {pokemon.map((pokemon, i)=>{
+                return(
+                    <li style={myStyle}>
+                    <a  href={`./pokemon${i}`}style={myStyle}>{(pokemon.name)}</a>
+                    </li>
+                )
+            })}
+        </ul>
 
         </>
         );
